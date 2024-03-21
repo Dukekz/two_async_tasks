5fb9b8a78e5e119305699a99e1194dbdd5b00c3e
Dukekz <dukekz2@gmail.com>
2024-03-21 17:12:35 +0500

    fix code

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+from typing import Coroutine, List
+
+
+async def run_first_service():
+    asyncio.sleep(1)
+    print("First service is run forever, press Ctrl+C to stop")
+    await asyncio.Future()
+    
+
+async def run_second_service():
+    asyncio.sleep(1)
+    print("Second service is run forever, press Ctrl+C to stop")
+    await asyncio.Future()
+    
+
+async def main(tasks: List[Coroutine]):
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+if __name__ == "__main__":
+    try:
+        tasks = [run_first_service(), run_second_service()]
+        with asyncio.Runner() as runner:
+            runner.run(main(tasks))
+    except KeyboardInterrupt:
+        pass
